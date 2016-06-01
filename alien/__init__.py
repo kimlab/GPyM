2d45c2883d00117eb88068beb855cb1d1ce93b4e
Hyungjun Kim <hyungjun@gmail.com>
2016-06-01 10:45:02 +0900

    [ADD] __init__.py @ alien

---
+++ alien/__init__.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+#--------------------------------------------------------------------
+# PROGRAM    : __init__.py	
+# CREATED BY : hjkim @IIS.2016-06-01 10:44:37.888292
+# MODIFED BY :
+#
+# USAGE      : $ ./__init__.py
+#
+# DESCRIPTION:
+#------------------------------------------------------cf0.2@20120401
+
+
+import  os,sys
+from    optparse        import OptionParser
+from    cf.util.LOGGER  import *
+
+
+@ETA
+def main(args,opts):
+    print args
+    print opts
+
+    return
+
+
+if __name__=='__main__':
+    usage   = 'usage: %prog [options] arg'
+    version = '%prog 1.0'
+
+    parser  = OptionParser(usage=usage,version=version)
+
+#    parser.add_option('-r','--rescan',action='store_true',dest='rescan',
+#                      help='rescan all directory to find missing file')
+
+    (options,args)  = parser.parse_args()
+
+#    if len(args) == 0:
+#        parser.print_help()
+#    else:
+#        main(args,options)
+
+#    LOG     = LOGGER()
+    main(args,options)
+
+    
