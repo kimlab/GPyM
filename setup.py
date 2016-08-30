27d93e726a07dff552bee392f7f9ebebfa9b6f31
Hyungjun Kim <hyungjun@gmail.com>
2016-08-30 18:31:28 +0900

    updating packaging

--- setup.py
+++ setup.py
@@ -15,17 +15,25 @@ setupFile.close()
 setup( name                 = 'GPyM',
        version              = '0.52b',
        description          = 'GPM Python Module',
+       long_description     = ''' long_description to be written. ''',
+
+       classifiers          = [
+                            'Development Status :: 2 - Beta',
+                            'License :: OSI Approved :: MIT License',
+                            'Programming Language :: Python :: 2.7',
+                            'Topic :: Scientific/Engineering :: Hydrology',
+                            ],
+       keywords             = 'precipitation satellite gpm trmm jaxa',
+       url                  = 'https://github.com/kimlab/GPyM',
        author               = 'Hyungjun Kim',
        author_email         = 'hyungjun@gmail.com',
-       url                  = 'https://github.com/kimlab/GPyM',
-       #package_dir          = {'GPyM':'./'},
+       license              = 'MIT',
+
        package_dir          = {'GPyM':''},
        packages             = ['GPyM','GPyM.alien'],
-       #install_requires     = ['numpy'],
-
-       #include_package_data = True,
-       data_files           = [( 'GPyM',['config'] ),
-                            ],
-
-       license              = 'MIT',
+       package_data         = {'': ['config'],
+                            },
+       install_requires     = ['numpy'],
+       include_package_data = True,
+       zip_safe             = True,
       )
