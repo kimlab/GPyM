0530ffc13e7085201fab2412f18e518805916e54
Hyungjun Kim <hyungjun@gmail.com>
2016-08-30 17:06:29 +0900

    updating packaging

--- settings.py
+++ settings.py
@@ -1,1 +1,5 @@
-baseDir = "/tank/hjkim/GPM"
+import  os, sys
+
+class env( object ):
+    trmm = os.path.join( os.environ[ 'HOME' ], 'GPyM' )
+    trmm = os.path.join( os.environ[ 'HOME' ], 'GPyM' )
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
-gpmDir  = ''
+gpmDir  = 'default'
 #gpmDir  = '/tank/hjkim/GPM'
 
 if gpmDir == '':
+    print gpmDir
     raise ValueError, 'gpmDir should be specificed'
 
 setupFile   = open('settings.py','w')
@@ -11,7 +12,7 @@ setupFile.write('baseDir = "%s"\n'%gpmDir)
 setupFile.close()
 
 setup( name                 = 'GPyM',
-       version              = '0.5',
+       version              = '0.52b',
        description          = 'GPM Python Module',
        author               = 'Hyungjun Kim',
        author_email         = 'hyungjun@gmail.com',
