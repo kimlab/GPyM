86336eb0c691c590f28449a849e7ac8b43ec4697
Hyungjun Kim <hyungjun@gmail.com>
2016-09-04 16:59:59 +0900

    [DEBUG] parse dtime of GPM/core granule over 2-day (e.g., GPMCOR_GMI_1404032357_0129_000550_L2S_GL2_03C.h5)

--- parse_fname_gpm.py
+++ parse_fname_gpm.py
@@ -13,7 +13,7 @@
 import  os,sys
 from    optparse        import OptionParser
 
-from    datetime        import datetime
+from    datetime        import datetime, timedelta
 
 
 def parse_fname_gpm(fName, ATTR):
@@ -27,6 +27,9 @@ def parse_fname_gpm(fName, ATTR):
     dictFunc= {'sDTime': datetime.strptime(fName[2], '%y%m%d%H%M'),
                'eDTime': datetime.strptime(fName[2][:6]+fName[3], '%y%m%d%H%M')
                }
+
+    if dictFunc['eDTime'] < dictFunc['sDTime']:
+        dictFunc['eDTime'] += timedelta( days=1 )
 
     return [dictFunc[attr] for attr in ATTR]
 
